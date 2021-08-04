ba17c9235badf7d30a25b2277188622a77cffec1
RomainWirth <wirth.romain@gmail.com>
2021-08-04 16:57:00 +0200

    fin implémentation authentification

--- routes/stuff.js
+++ routes/stuff.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const stuffCtrl = require('../controllers/stuff');
+const auth = require('../middleware/auth');
 
-router.get('/', stuffCtrl.getAllSauces); // trouver toutes les sauces
-router.get('/:id', stuffCtrl.getOneSauce); // trouver une sauce en particulier
-router.post('/', stuffCtrl.createSauce); // nouvelle sauce 
-router.put('/:id', stuffCtrl.modifySauce); // modifier une sauce
-router.delete('/:id', stuffCtrl.deleteSauce); // supprimer une sauce
+router.get('/', auth, stuffCtrl.getAllSauces); // trouver toutes les sauces
+router.get('/:id', auth, stuffCtrl.getOneSauce); // trouver une sauce en particulier
+router.post('/', auth, stuffCtrl.createSauce); // nouvelle sauce 
+router.put('/:id', auth, stuffCtrl.modifySauce); // modifier une sauce
+router.delete('/:id', auth, stuffCtrl.deleteSauce); // supprimer une sauce
 
 module.exports = router;
