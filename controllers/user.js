6993842ca49759e7fa2f50bd6e2282e1523d4522
RomainWirth <wirth.romain@gmail.com>
2021-08-14 14:37:15 +0200

    modif commentaires

--- controllers/user.js
+++ controllers/user.js
@@ -1,16 +1,16 @@
 const bcrypt = require('bcrypt'); // Algorythme de hachage = package de chiffrement
 const jwt = require('jsonwebtoken'); // standard qui permet l'échange de jetons
 
-const User = require('../models/User');
+const User = require('../models/User'); // récupéré du modèle User.js avec uniqueValidator : on ne peut pas utiliser deux fois la même adresse email
 
 exports.signup = (req, res, next) => {
-    bcrypt.hash(req.body.password, 10) // salt (salage) : 10 tours d'algorythme de hachage suffisent pour la sécurité et éviter que ce soit trop long
+    bcrypt.hash(req.body.password, 10) // hash : salt = 10 tours d'algorythme de hashage suffisent pour la sécurité et éviter que ce soit trop long
     .then(hash => { // hash = méthode pour crypter le mot de passe
         const user = new User({
-            email: req.body.email,
-            password: hash
+            email: req.body.email, // adresse email du corps de la requête
+            password: hash // le mot de passe est stocké crypté
         });
-        user.save()
+        user.save() // enregistrement du user dans la BDD
         .then(() => res.status(201).json({ message: 'utilisateur créé'}))
         .catch(error => res.status(400).json({ error }));
     })
